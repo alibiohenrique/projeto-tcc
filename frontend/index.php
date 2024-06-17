386b47bace674d0afe1a83e108a7a46dbca63a59
unknown <eventos@UNR02DT75471.animaedu.intranet>
2024-06-17 10:17:35 -0300

    feat: created utils and components

---
+++ frontend/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/components/header.css">
+    <link rel="stylesheet" href="styles/utils/utils.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>.</title>
+</head>
+
+<body>
+
+    <header>
+        <section class="section">
+
+            <button class="btn btn1">
+                button
+            </button>
+            <div class="pop-up">
+                <h2>
+                    Título
+                </h2>
+                <h3>
+                    Sub-título
+                </h3>
+                <p>
+                    Mensagem
+                    Mensagem
+                    Mensagem
+                    Mensagem
+                    Mensagem
+                    Mensagem
+                    Mensagem
+                    Mensagem
+                    Mensagem
+                </p>
+            </div>
+        </section>
+
+    </header>
+
+</body>
+
+</html>
