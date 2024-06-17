7df1f181828cdc64d2181f05eb3355673f86df7b
Gabriel O. Ferreira <107577067+GFerrDev@users.noreply.github.com>
2024-06-17 11:54:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/components/header.css">
     <link rel="stylesheet" href="styles/utils/utils.css">
-    <link rel="stylesheet" href="styles/navBar
-    .css">
+    <link rel="stylesheet" href="styles/utils/navBar.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>kidscript</title>
