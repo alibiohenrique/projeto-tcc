548b4fd7639654f5cac39a884444d699ba713d43
GFferr <gabriel.oferreira@outlook.com.br>
2024-06-24 10:56:03 -0300

    implrmrntação inicial html landing page

--- frontend/index.php
+++ frontend/index.php
@@ -78,13 +78,35 @@
 
     <main>
         <div class="section" id="LPSection">
-            <div class="textSection" id="LPSection">
+            <div class="textSection">
                 <h2>algum titulo <br><span>aqui mesmo</span></h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
                 <div class="btnSection"><button class="btn">Entrar</button> <button class="btn">cadastre-se</button></div>
             </div>
             <iframe src="https://www.youtube.com/embed/wMTnjSJGWCs?si=ZkHGBrhkyhx6DwqY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
+        <div class="textSection">
+            <h2> aprenda jogando</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
+
+        </div>
+
+        <div class="section" id="LPSection2">
+            <div class="card">
+                <div class="cHead">
+                    <div class="textSection">
+                        <h2>Nome do expeecialista</h2>
+                        <p>Expecialidades, crgos e etc aqui</p>
+                    </div>
+                    <img src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719187200&semt=ais_user" alt="">
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe suscipit ullam. Obcaecati, ad? Natus voluptas consequuntur non, officia officiis illum corporis? Tempora recusandae ipsam culpa doloremque aliquam necessitatibus animi?</p>
+            </div>
+        </div>
+
         <div class="pop-up">
             <div class="exit-btn">
 
