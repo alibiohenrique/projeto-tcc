2c6c52d5f4f9842a1034678e37a2d6e105cf6671
GFferr <gabriel.oferreira@outlook.com.br>
2024-06-25 11:57:25 -0300

    landing page update

--- frontend/index.php
+++ frontend/index.php
@@ -9,11 +9,13 @@
     <!-- HTML5 default configs -->
 
     <!-- Page styles -->
-    <link rel="stylesheet" href="styles/components/header.css">
-    <link rel="stylesheet" href="styles/components/main.css">
     <link rel="stylesheet" href="styles/utils/utils.css">
+    <link rel="stylesheet" href="styles/components/header.css">
     <link rel="stylesheet" href="styles/utils/navbar.css">
+    <link rel="stylesheet" href="styles/components/main.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <!-- Page styles -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 </head>
 
 <body>
@@ -21,41 +23,33 @@
         <img src="assets/logo.png" alt="Logo Kidscript" class="logo">
         <div class="nav">
             <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-house-fill"
-                    viewBox="0 0 16 16">
-                    <path
-                        d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
-                    <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#5ce1e6" class="bi bi-house-fill" viewBox="0 0 16 16>
+                    <path d=" M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
+                <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
                 </svg>
                 <span>Home</span>
             </a>
-            <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-exclamation-circle"
-                    viewBox="0 0 16 16">
+            <a href="about.php">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                    <path
-                        d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
+                    <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
                 </svg>
                 <span>Informações</span>
             </a>
             <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-play-circle-fill"
-                    viewBox="0 0 16 16">
-                    <path
-                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z" />
                 </svg>
                 <span>Play</span>
             </a>
             <a href="">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
-                    <path
-                        d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+                    <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
                 </svg>
                 <span>Configurações</span>
             </a>
             <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-fill"
-                    viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
                 </svg>
                 <span>Perfil</span>
@@ -66,30 +60,21 @@
 
 
     <nav>
-        <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-house-fill"
-                viewBox="0 0 16 16">
-                <path
-                    d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
+        <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="#048A81" class="bi bi-house-fill" viewBox="0 0 16 16" color="var(--lite-blue)">
+                <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
                 <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
             </svg></a>
-        <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-exclamation-circle"
-                viewBox="0 0 16 16">
+        <a href="about.php"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                <path
-                    d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
+                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
             </svg></a>
-        <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-play-circle-fill"
-                viewBox="0 0 16 16">
-                <path
-                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z" />
+        <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z" />
             </svg></a>
-        <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-gear-fill"
-                viewBox="0 0 16 16">
-                <path
-                    d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+        <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+                <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
             </svg></a>
-        <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-fill"
-                viewBox="0 0 16 16">
+        <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
             </svg></a>
     </nav>
@@ -99,64 +84,83 @@
             <div class="textSection">
                 <h2>algum titulo <br><span>aqui mesmo</span></h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
-                <div class="btnSection"><button class="btn">Entrar</button> <button class="btn">cadastre-se</button>
+                <div class="btnSection" id="btnS"><button class="btn">Entrar</button> <button class="btn btn1">cadastre-se</button>
                 </div>
             </div>
-            <iframe src="https://www.youtube.com/embed/wMTnjSJGWCs?si=ZkHGBrhkyhx6DwqY" title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe src="https://www.youtube.com/embed/wMTnjSJGWCs?si=ZkHGBrhkyhx6DwqY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <div class="textSection">
             <h2> aprenda jogando</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
+            <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
+            <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
+            <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
+            <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique ipsam.</p>
 
         </div>
 
         <div class="section" id="LPSection2">
-            <div class="card">
+            <div data-aos="fade-up" class="card">
                 <div class="cHead">
                     <div class="textSection">
                         <h2>Nome do expeecialista</h2>
-                        <p>Expecialidades, crgos e etc aqui</p>
+                        <p>Expecialidades, cargos e etc aqui</p>
                     </div>
                     <img src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719187200&semt=ais_user" alt="">
                 </div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe suscipit ullam. Obcaecati, ad? Natus voluptas consequuntur non, officia officiis illum corporis? Tempora recusandae ipsam culpa doloremque aliquam necessitatibus animi?</p>
             </div>
+            <div data-aos="fade-up" class="card">
+                <div class="cHead">
+                    <div class="textSection">
+                        <h2>Nome do expeecialista</h2>
+                        <p>Expecialidades, cargos e etc aqui</p>
+                    </div>
+                    <img src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719187200&semt=ais_user" alt="">
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe suscipit ullam. Obcaecati, ad? Natus voluptas consequuntur non, officia officiis illum corporis? Tempora recusandae ipsam culpa doloremque aliquam necessitatibus animi?</p>
+            </div>
+            <div data-aos="fade-up" class="card">
+                <div class="cHead">
+                    <div class="textSection">
+                        <h2>Nome do expeecialista</h2>
+                        <p>Expecialidades, cargos e etc aqui</p>
+                    </div>
+                    <img src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719187200&semt=ais_user" alt="">
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe suscipit ullam. Obcaecati, ad? Natus voluptas consequuntur non, officia officiis illum corporis? Tempora recusandae ipsam culpa doloremque aliquam necessitatibus animi?</p>
+            </div>
+            <div class="btnSection bottom"><button class="btn">Entrar</button> <button class="btn btn1">cadastre-se</button>
+            </div>
         </div>
 
         <div class="pop-up">
-                <div class="close-button">
-                    <a href="Close event">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                        class="bi bi-x-lg" viewBox="0 0 16 16">
-                        <path
-                            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+            <div class="close-button">
+                <a href="Close event">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
                     </svg>
-                    </a>
-                </div>
-
-                <h2>
-                    Title
-                </h2>
-
-                <h3>
-                    Sub-title
-                </h3>
-
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo autem neque non. Possimus ab ducimus
-                    quia doloribus ex qui, excepturi corporis, odit sunt placeat illo neque magnam labore, perspiciatis
-                    dolorem!
-                </p>
+                </a>
             </div>
+
+            <h2>
+                Title
+            </h2>
+
+            <h3>
+                Sub-title
+            </h3>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo autem neque non. Possimus ab ducimus
+                quia doloribus ex qui, excepturi corporis, odit sunt placeat illo neque magnam labore, perspiciatis
+                dolorem!
+            </p>
         </div>
     </main>
 
+    <script>
+        AOS.init();
+    </script>
 </body>
 
 </html>
