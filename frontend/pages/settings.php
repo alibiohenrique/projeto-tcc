0f57c1ef6415955301473f27fcf2bd38c7bdd64c
nPreissler <nestorpreissler@gmail.com>
2024-06-27 09:44:56 -0300

    test: global user change

--- frontend/pages/settings.php
+++ frontend/pages/settings.php
@@ -5,7 +5,7 @@
     <!-- HTML5 & page default configs -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
-    <title> KidScript | Início </title>
+    <title> KidScript | Configurações </title>
     <!-- HTML5 default configs -->
 
     <!-- Page styles -->
