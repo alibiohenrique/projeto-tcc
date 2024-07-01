9acab4243e046ee39de2c37f884926c7a8143f10
unknown <eventos@UNR02DT75484.animaedu.intranet>
2024-07-01 08:35:37 -0300

    fixed: merged

--- frontend/index.php
+++ frontend/index.php
@@ -1,4 +1,3 @@
-
 <head>
     <!-- HTML5 & page default configs -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,10 +11,10 @@
     <!-- HTML5 default configs -->
 
     <!-- Page styles -->
+    <link rel="stylesheet" href="styles/utils/utils.css">
     <link rel="stylesheet" href="styles/components/header.css">
     <link rel="stylesheet" href="styles/components/main.css">
-    <link rel="stylesheet" href="styles/utils/utils.css">
-    <!-- <link rel="stylesheet" href="styles/utils/navbar.css"> -->
+
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <!-- Page styles -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
@@ -72,26 +71,22 @@
     </header>
 
     <main>
-        <div class="section" id="LPSection">
+        <section class="section" id="LPSection">
             <div class="textSection">
                 <h2>algum titulo <br><span>aqui mesmo</span></h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
-                <div class="btnSection" id="btnS"><button class="btn">Entrar</button> <button
-                        class="btn btn1">cadastre-se</button>
-                <div class="btnSection" id="btnS"><button class="btn">Entrar</button> <button
-                        class="btn btn1">cadastre-se</button>
+                <div class="btnSection" id="btnS">
+                    <button class="btn">Entrar</button>
+                    <button class="btn btn1">cadastre-se</button>
                 </div>
-            </div>
-            <iframe src="https://www.youtube.com/embed/wMTnjSJGWCs?si=ZkHGBrhkyhx6DwqY" title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-            <iframe src="https://www.youtube.com/embed/wMTnjSJGWCs?si=ZkHGBrhkyhx6DwqY" title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
-        <div class="textSection">
+                <iframe src="https://www.youtube.com/embed/wMTnjSJGWCs?si=ZkHGBrhkyhx6DwqY" title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                </iframe>
+
+        </section>
+        <section class="section textSection">
             <h2> aprenda jogando</h2>
             <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet
                 consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt
@@ -117,34 +112,10 @@
                 amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium
                 magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique
                 ipsam.</p>
-            <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt
-                nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit
-                amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium
-                magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique
-                ipsam.</p>
-            <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt
-                nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit
-                amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium
-                magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique
-                ipsam.</p>
-            <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt
-                nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit
-                amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium
-                magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique
-                ipsam.</p>
-            <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. At eaque repellendus, error alias quos nihil vitae voluptatem incidunt
-                nobis? Eum enim fugit consequatur ad repudiandae expedita rem fugiat qui mollitia. Lorem ipsum dolor sit
-                amet consectetur adipisicing elit. Voluptatem aut fugiat assumenda quod debitis laboriosam praesentium
-                magni obcaecati a tenetur rem aperiam corrupti dolore, perspiciatis voluptatum numquam ea similique
-                ipsam.</p>
 
-        </div>
+        </section>
 
-        <div class="section" id="LPSection2">
+        <section class="section" id="LPSection2">
             <div data-aos="fade-up" class="card">
                 <div class="cHead">
                     <div class="textSection">
@@ -153,15 +124,12 @@
                     </div>
                     <img src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719187200&semt=ais_user"
                         alt="">
-                    <img src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719187200&semt=ais_user"
-                        alt="">
+
                 </div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe suscipit ullam. Obcaecati, ad?
                     Natus voluptas consequuntur non, officia officiis illum corporis? Tempora recusandae ipsam culpa
                     doloremque aliquam necessitatibus animi?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe suscipit ullam. Obcaecati, ad?
-                    Natus voluptas consequuntur non, officia officiis illum corporis? Tempora recusandae ipsam culpa
-                    doloremque aliquam necessitatibus animi?</p>
+
             </div>
             <div data-aos="fade-up" class="card">
                 <div class="cHead">
@@ -171,15 +139,12 @@
                     </div>
                     <img src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719187200&semt=ais_user"
                         alt="">
-                    <img src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719187200&semt=ais_user"
-                        alt="">
+
                 </div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe suscipit ullam. Obcaecati, ad?
                     Natus voluptas consequuntur non, officia officiis illum corporis? Tempora recusandae ipsam culpa
                     doloremque aliquam necessitatibus animi?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe suscipit ullam. Obcaecati, ad?
-                    Natus voluptas consequuntur non, officia officiis illum corporis? Tempora recusandae ipsam culpa
-                    doloremque aliquam necessitatibus animi?</p>
+
             </div>
             <div data-aos="fade-up" class="card">
                 <div class="cHead">
@@ -189,52 +154,20 @@
                     </div>
                     <img src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719187200&semt=ais_user"
                         alt="">
-                    <img src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1719187200&semt=ais_user"
-                        alt="">
+                    
                 </div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe suscipit ullam. Obcaecati, ad?
                     Natus voluptas consequuntur non, officia officiis illum corporis? Tempora recusandae ipsam culpa
                     doloremque aliquam necessitatibus animi?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe suscipit ullam. Obcaecati, ad?
-                    Natus voluptas consequuntur non, officia officiis illum corporis? Tempora recusandae ipsam culpa
-                    doloremque aliquam necessitatibus animi?</p>
-            </div>
-            <div class="btnSection bottom"><button class="btn">Entrar</button> <button
-                    class="btn btn1">cadastre-se</button>
-            <div class="btnSection bottom"><button class="btn">Entrar</button> <button
-                    class="btn btn1">cadastre-se</button>
-            </div>
-        </div>
 
-        <!-- <div class="pop-up">
-            <div class="close-button">
-                <a href="Close event">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                        class="bi bi-x-lg" viewBox="0 0 16 16">
-                        <path
-                            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                        class="bi bi-x-lg" viewBox="0 0 16 16">
-                        <path
-                            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-                    </svg>
-                </a>
+            </div>
+            <div class="btnSection bottom">
+                <button class="btn">Entrar</button>
+                <button class="btn btn1">cadastre-se</button>
+
             </div>
 
-            <h2>
-                Title
-            </h2>
-
-            <h3>
-                Sub-title
-            </h3>
-
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo autem neque non. Possimus ab ducimus
-                quia doloribus ex qui, excepturi corporis, odit sunt placeat illo neque magnam labore, perspiciatis
-                dolorem!
-            </p>
-        </div> -->
+        </section>
     </main>
 
 
